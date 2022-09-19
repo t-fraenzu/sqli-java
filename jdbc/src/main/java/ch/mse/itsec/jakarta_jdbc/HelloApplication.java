7a553744e6093b56bc339c102d288f10af485dbc
Tizian Koller <kollti@akros.ch>
2022-09-19 19:37:08 +0200

    setup basic jakata ee web project

---
+++ jdbc/src/main/java/ch/mse/itsec/jakarta_jdbc/HelloApplication.java
@@ -0,0 +1,9 @@
+package ch.mse.itsec.jakarta_jdbc;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
